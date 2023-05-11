b831c91e4717f45113c3e16a857ed9412555dbe4
Martin Henry <martin.henry@hashicorp.com>
2023-05-11 14:02:00 -0700

    cleanup

--- addon/instance-initializers/load-svg.js
+++ addon/instance-initializers/load-svg.js
@@ -1,11 +1,6 @@
-/* eslint-disable prettier/prettier */
-/* global Prism */
-// const flightIconSprite = require('node_modules/@hashicorp/flight-icons/svg-sprite/svg-sprite-module');
 import * as svgSprite from '@hashicorp/flight-icons/svg-sprite/svg-sprite-module';
 
 export function initialize(/* application */) {
-  // Overriding `unhashHTMLSpans` subparser to overcome the 10 levels of nesting limit
-  console.log('initialize here')
   window.document.body.insertAdjacentHTML('beforeend', svgSprite)
 }
 
