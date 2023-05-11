782b1efbec5f1f409680b6f689dde7580f908c33
Martin Henry <martin.henry@hashicorp.com>
2023-05-10 17:29:29 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# ember-flight-icons
+
+[![npm version](https://badge.fury.io/js/%40hashicorp%2Fember-flight-icons.svg)](https://badge.fury.io/js/%40hashicorp%2Fember-flight-icons)
+
+[![CI - Ember Flight Icons](https://github.com/hashicorp/design-system/actions/workflows/ci-ember-flight-icons.yml/badge.svg)](https://github.com/hashicorp/design-system/actions/workflows/ci-ember-flight-icons.yml)
+
+An Ember addon for the icons from `flight`.
+
+Goals:
+
+* Accessible
+* Easy to use
+* Easy to maintain
+* Sensible defaults
+
+## Compatibility
+
+* Ember.js v3.28 or above
+  * Note: The library _should_ work with earlier versions of Ember, but we only test with Ember 3.28 and newer
+* Ember CLI v3.28 or above
+* Node.js v12 or above
+
+## Installation and Usage
+
+See [https://flight-hashicorp.vercel.app/](https://flight-hashicorp.vercel.app/) for installation and usage instructions.
+
+## Contributing
+
+See the [Contributing](CONTRIBUTING.md) guide for details.
+
+## License
+
+This project is licensed under the [Mozilla Public License 2.0](LICENSE.md).
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning.
---
+++ config/ember-try.js
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: MPL-2.0
+ */
+
+'use strict';
+
+const getChannelURL = require('ember-source-channel-url');
+const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
+
+module.exports = async function () {
+  return {
+    useYarn: true,
+    // override this to avoid ember-try trying to use `--no-lockfile` which
+    // doesn't exist in yarn3
+    // see https://github.com/ember-cli/ember-try/issues/74
+    buildManagerOptions() {
+      return [''];
+    },
+    scenarios: [
+      {
+        name: 'ember-lts-3.24',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.24.3',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-3.28',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.28.0',
+          },
+        },
+      },
+      {
+        name: 'ember-release',
+        npm: {
+          devDependencies: {
+            'ember-source': await getChannelURL('release'),
+          },
+        },
+      },
+      {
+        name: 'ember-beta',
+        npm: {
+          devDependencies: {
+            'ember-source': await getChannelURL('beta'),
+          },
+        },
+      },
+      {
+        name: 'ember-canary',
+        npm: {
+          devDependencies: {
+            'ember-source': await getChannelURL('canary'),
+          },
+        },
+      },
+      {
+        name: 'ember-classic',
+        env: {
+          EMBER_OPTIONAL_FEATURES: JSON.stringify({
+            'application-template-wrapper': true,
+            'default-async-observers': false,
+            'template-only-glimmer-components': false,
+          }),
+        },
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.28.0',
+          },
+          ember: {
+            edition: 'classic',
+          },
+        },
+      },
+      embroiderSafe(),
+      embroiderOptimized(),
+    ],
+  };
+};
